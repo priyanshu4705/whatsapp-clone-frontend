b2de039dca5b277497894eb25ad62c892e68afaa
priyanshu4705 <priyanshusrivastava4705@gmail.com>
2021-02-17 22:17:38 +0530

    deployed

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,9 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import { firebaseConfig } from './config';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDvpFF6KKYsdI4JfkWEpRRR0F5-BSe2Tt0",
-    authDomain: "whatsappclonefrontend.firebaseapp.com",
-    projectId: "whatsappclonefrontend",
-    storageBucket: "whatsappclonefrontend.appspot.com",
-    messagingSenderId: "203113435978",
-    appId: "1:203113435978:web:3626e2d5765f7fa8eda7b3",
-    measurementId: "G-WDYPYGXZ6W"
-};
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
